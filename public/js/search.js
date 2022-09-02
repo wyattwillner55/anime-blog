47e56cac5be0929c82001bfdaba397ccaf065cb0
wyattwillner55 <109093691+wyattwillner55@users.noreply.github.com>
2022-09-01 22:55:29 -0400

    Add files via upload

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let searchBtnEl = document.querySelector('#search-btn');
 const searchInputEl = document.querySelector('#search-input');
 const homepageURL = window.location.href;
@@ -8,7 +10,7 @@ let searchData;
 //and assigns the homepage as the location when search button is pressed
 function searchAndDisplay() {
     location.assign(homepageURL);
-    searchAnime(filterInput(searchInputEl.textContent));
+    searchAnime(filterInput(searchInputEl.value));
     displaySearch();
 }
 
@@ -19,7 +21,7 @@ function searchAndDisplay() {
 }*/
 
 //fetches the top anime from jikan api
-async function getTopAnime(){
+/*async function getTopAnime(){
     const response = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
     topAnime = await response.json();
     console.log(topAnime);
@@ -29,12 +31,20 @@ async function getTopAnime(){
         window.localStorage.setItem('topAnime' + i , JSON.stringify(topAnime[i]));
     }
 }
+*/
 
 //searches the api for the anime
-async function searchAnime(searchedAnime) {
-    const response = await fetch('https://api.jikan.moe/v3/search/anime?q=' + searchedAnime + '&order_by=title&sort=asc&limit=1')
-       searchData = await response.json();
-    window.localStorage.setItem('searchedAnime' , JSON.stringify(searchData));
+function searchAnime(searchedAnime) {
+    fetch('https://api.jikan.moe/v3/search/anime?q=' + searchedAnime + '&order_by=title&sort=asc&limit=1')
+        .then(function(response){
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function(searchedData){
+            window.localStorage.setItem('searchedAnime' , JSON.stringify(searchedData));
+        })
 }
 
 //replaces unsearchable characters in the string with characters that can be used in a search
@@ -55,6 +65,7 @@ function displaySearch() {
 }
 
 //displays the top anime in their locations
+/*
 function displayHomepageAnime(animeNumber){
     const animeInfo = JSON.parse(window.localStorage.getItem('topAnime' + animeNumber));
     let animeObj = `${animeNumber}`;
@@ -67,8 +78,9 @@ function displayHomepageAnime(animeNumber){
     document.getElementById(animeObj).setAttribute('target' , animeInfo.title);
     document.getElementById(animeImage).setAttribute('src' , animeInfo.image_url);
 }
-
+*/
 //clears display
+/*
 function clearDisplayData() {
     document.querySelector('#aTitle').textContent = 'Title:' + '';
     document.querySelector('#synopsis').textContent = 'Synopsis:' + '';
@@ -78,5 +90,4 @@ function clearDisplayData() {
     document.querySelector('#malUrl').setAttribute('target' , '');
     document.querySelector('#aPoster').setAttribute('src' , '');
 }
-
-searchBtnEl.addEventListener('click', searchAndDisplay());
+*/
