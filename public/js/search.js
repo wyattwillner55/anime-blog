2d1eb339d27e1c2b1c23e91738172fab5f187dc7
wyattwillner55 <109093691+wyattwillner55@users.noreply.github.com>
2022-09-01 20:04:25 -0400

    Add files via upload

--- public/js/search.js
+++ public/js/search.js
@@ -15,28 +15,26 @@ function searchAndDisplay() {
 //function that calls all the relevant functions for setting up the homepage top anime display
 function setUpHomepage() {
     getTopAnime();
-    for (i = 0; i < 4; i++) {
-        displayHomepageAnime(i);
-    }
-    /*display0();
-    display1(); 
-    display2();
-    display3();
-    display4();*/
+    
 }
 
 //fetches the top anime from jikan api
 async function getTopAnime(){
     const response = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
     topAnime = await response.json();
-    topAnime = JSON.parse(topAnime.top.slice(0,5));
+    console.log(topAnime);
+    topAnime = topAnime.top.slice(0,5);
+    for (i = 0; i < 4; i++) {
+        console.log('topAnime' + i);
+        window.localStorage.setItem('topAnime' + i , JSON.stringify(topAnime[i]));
+    }
 }
 
 //searches the api for the anime
 async function searchAnime(searchedAnime) {
     const response = await fetch('https://api.jikan.moe/v3/search/anime?q=' + searchedAnime + '&order_by=title&sort=asc&limit=1')
        searchData = await response.json();
-    
+    window.localStorage.setItem('searchedAnime' , JSON.stringify(searchData));
 }
 
 //replaces unsearchable characters in the string with characters that can be used in a search
@@ -46,59 +44,29 @@ function filterInput(inputString) {
 
 //displays the searched anime
 function displaySearch() {
-    document.querySelector('#aTitle').textContent = 'Title:' + searchData.title;
-    document.querySelector('#synopsis').textContent = 'Synopsis:' + searchData.synopsis;
-    document.querySelector('#episodes').textContent = 'Episodes:' + searchData.episodes;
-    document.querySelector('#malScore').textContent = 'Rating:' + searchData.score;
-    document.querySelector('#malUrl').setAttribute('href' , searchData.url);
-    document.querySelector('#malUrl').setAttribute('target' , searchData.title);
-    document.querySelector('#aPoster').setAttribute('src' , searchData.image_url);
+    const searchedAnime = JSON.parse(window.localStorage.getItem('searchedAnime'));
+    document.querySelector('#aTitle').textContent = 'Title:' + searchedAnime.title;
+    document.querySelector('#synopsis').textContent = 'Synopsis:' + searchedAnime.synopsis;
+    document.querySelector('#episodes').textContent = 'Episodes:' + searchedAnime.episodes;
+    document.querySelector('#malScore').textContent = 'Rating:' + searchedAnime.score;
+    document.querySelector('#malUrl').setAttribute('href' , searchedAnime.url);
+    document.querySelector('#malUrl').setAttribute('target' , searchedAnime.title);
+    document.querySelector('#aPoster').setAttribute('src' , searchedAnime.image_url);
 }
 
 //displays the top anime in their locations
 function displayHomepageAnime(animeNumber){
+    const animeInfo = JSON.parse(window.localStorage.getItem('topAnime' + animeNumber));
     let animeObj = `${animeNumber}`;
     let animeImage = `${animeNumber}` + 'i';
     console.log(animeNumber);
     console.log(topAnime[animeNumber].url);
     console.log(topAnime[animeNumber].title);
     console.log(topAnime[animeNumber].image);
-    document.getElementById(animeObj).setAttribute('href' , topAnime[animeNumber].url);
-    document.getElementById(animeObj).setAttribute('target' , topAnime[animeNumber].title);
-    document.getElementById(animeImage).setAttribute('src' , topAnime[animeNumber].image_url);
+    document.getElementById(animeObj).setAttribute('href' , animeInfo.url);
+    document.getElementById(animeObj).setAttribute('target' , animeInfo.title);
+    document.getElementById(animeImage).setAttribute('src' , animeInfo.image_url);
 }
-
-function display0() {
-    document.getElementById('0').setAttribute('href' , topAnime[0].url);
-    document.getElementById('0').setAttribute('target' , topAnime[0].title);
-    document.getElementById('0i').setAttribute('src' , topAnime[0].image_url);
-}
-
-function display1() {
-    document.getElementById('1').setAttribute('href' , topAnime[1].url);
-    document.getElementById('1').setAttribute('target' , topAnime[1].title);
-    document.getElementById('1i').setAttribute('src' , topAnime[1].image_url);
-}
-
-function display2() {
-    document.getElementById('2').setAttribute('href' , topAnime[2].url);
-    document.getElementById('2').setAttribute('target' , topAnime[2].title);
-    document.getElementById('2i').setAttribute('src' , topAnime[2].image_url);
-}
-
-function display3() {
-    document.getElementById('3').setAttribute('href' , topAnime[3].url);
-    document.getElementById('3').setAttribute('target' , topAnime[3].title);
-    document.getElementById('3i').setAttribute('src' , topAnime[3].image_url);
-}
-
-function display4() {
-    document.getElementById('4').setAttribute('href' , topAnime[4].url);
-    document.getElementById('4').setAttribute('target' , topAnime[4].title);
-    document.getElementById('4i').setAttribute('src' , topAnime[4].image_url);
-}
-
-
 
 //clears display
 function clearDisplayData() {
